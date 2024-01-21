e45adf4429da7bd883e28ed1ce6a2473dccbd2ec
Raykei <inazumae26@gmail.com>
2024-01-21 14:42:34 -0500

    html connection

--- jetsonrobotwebcontroller/settings.py
+++ jetsonrobotwebcontroller/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'jetsonrobotwebcontroller.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["jetsonrobotwebcontroller/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
