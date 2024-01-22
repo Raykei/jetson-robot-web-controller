96d9c944500c6c96b966fdab10e30c69adb8c833
Raykei <naperezs@pucp.edu.pe>
2024-01-22 18:40:53 -0500

    Commiting from t480 :)

--- jetsonrobotwebcontroller/camera.py
+++ jetsonrobotwebcontroller/camera.py
@@ -3,7 +3,7 @@ import cv2
 class webcam(object):
     # Is called when an object is created
     def __init__(self):
-        self.video = cv2.VideoCapture(1)
+        self.video = cv2.VideoCapture(0)
         # self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         # self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
         if not self.video.isOpened():
